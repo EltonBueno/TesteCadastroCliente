dacc709cd803fd7928efa8e84e83c65e7b846a28
ELTON SENAC <elsbueno6@gmail.com>
2022-08-30 21:36:18 -0300

    Configuração do EFCore

--- CadastroClientes/Models/Cliente.cs
+++ CadastroClientes/Models/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,8 +16,15 @@ namespace CadastroClientes.Models
         }
 
         public int Id { get; private set; }
+
+        [Required(ErrorMessage = "Nome é obrigatório!")]
         public string Nome { get; private set; }
+
+        [Required(ErrorMessage = "Nascimento é obrigatório!")]
+        [Display(Name = "Data de Nascimento")]
         public DateTime Nascimento { get; private set; }
+
+        [Required(ErrorMessage = "Email é obrigatório!")]
         public string Email { get; private set; }
 
         public int Idade()
